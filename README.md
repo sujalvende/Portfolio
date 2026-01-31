4e5a0ae80a2a196fddede0ecb8f9603932560047
Sujal Vende <sujalvende9@gmail.com>
2026-01-31 14:03:31 +0530

    Fixing some bugs and adding some animations

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Portfolio
+
+THis is my portfolio. You can cheack by cliking here https://sujalvende.github.io/Portfolio/
--- script.js
+++ script.js
@@ -42,4 +42,70 @@ function borderCursor(ele){
 }
 
 let heroName = document.querySelector("#hero-name h1");
-borderCursor(heroName);
+// borderCursor(heroName);
+
+
+// Animation for nav section
+
+let t1 = gsap.timeline();
+
+t1.from("#logo",{
+    position: "relative",
+    top: "-10rem",
+    duration: 1,
+    ease: "Power3.easeOut",
+})
+
+t1.from("#nav-links a",{
+    position: "relative",
+    top: "-10rem",
+    duration: 1.2,
+    ease: "Power3.easeOut",
+    stagger: 0.4,
+});
+
+t1.from("#hero-name p",{
+    position: "relative",
+    left: "-20rem",
+    duration: 0.8,
+    ease: "Power3.easeOut",
+})
+
+t1.from("#hero-name h1",{
+    scale: 0,
+    duration: 0.8,
+    ease: "Elastic.easeOut.config(1, 0.5)",
+});
+
+t1.from("#hero-frontend h1",{
+    position: "relative",
+    left: "-95rem",
+    duration: 0.8,
+    ease: "linner",
+});
+
+t1.from("#hero-backend p",{
+    position: "relative",
+    right: "-100rem",
+    duration: 0.8,
+});
+
+t1.from("#hero-backend h1",{
+    position: "relative",
+    left: "1rem",
+    bottom: "-20rem",
+    duration: 0.8,
+});
+
+t1.from("#rotating-element",{
+    scale: 0,
+    duration: 2,
+    ease: "Power3.easeOut",
+});
+
+gsap.to("#rotating-element",{
+    rotation: 360,
+    repeat: -1,
+    duration: 1.5,
+    ease: "none",
+});
